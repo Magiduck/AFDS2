1cc84c5d0121c3710e5ab122cc8909418eb7eed0
Magiduck <tijsvanlieshout@gmail.com>
2019-06-17 16:25:57 +0200

    setup() fix and GUI for contacts

--- main.py
+++ main.py
@@ -34,97 +34,6 @@ class MainScreen(Screen):
     pass
 
 
-# class ContactsScreen(Screen):
-#
-#     def __init__(self, **kwargs):
-#         super(ContactsScreen, self).__init__(**kwargs)
-#         self.first_name_text_input = ObjectProperty(None)
-#         self.number_input = ObjectProperty(None)
-#         self.students_list_view = ObjectProperty(None)
-#
-#         Clock.schedule_once(self.setup)
-#
-#     def setup(self):
-#         with open('names&numbers.csv', mode='r') as names_numbers:
-#             student_reader = csv.reader(names_numbers, delimiter=',')
-#             for row in student_reader:
-#                 if row:
-#                     student_name = row[0] + " " + row[1]
-#                     self.ids.students_list_view.adapter.data.extend([student_name])
-#         self.ids.students_list_view._trigger_reset_populate()
-#
-#     def submit_student(self):
-#
-#         # Get the student name from the TextInputs
-#         student_name = self.first_name_text_input.text + " " + self.number_input.text
-#
-#         # Add the student to the ListView
-#         self.ids.students_list_view.adapter.data.extend([student_name])
-#
-#         # Reset the ListView
-#         self.ids.students_list_view._trigger_reset_populate()
-#
-#         with open('names&numbers.csv', mode='a') as names_numbers:
-#             student_writer = csv.writer(names_numbers, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#             # student_writer.writerow(['Name', 'Number'])
-#             student_writer.writerow([self.first_name_text_input.text, self.number_input.text])
-#
-#     def delete_student(self, *args):
-#
-#         # If a list item is selected
-#         if self.ids.students_list_view.adapter.selection:
-#
-#             # Get the text from the item selected
-#             selection = self.ids.students_list_view.adapter.selection[0].text
-#
-#             # Remove the matching item
-#             self.ids.students_list_view.adapter.data.remove(selection)
-#
-#             # Reset the ListView
-#             self.ids.students_list_view._trigger_reset_populate()
-#
-#             with open('names&numbers.csv', mode='r') as names_numbers, open('tempfile.csv', 'w') as tempfile:
-#                 student_reader = csv.reader(names_numbers, delimiter=',')
-#                 student_writer = csv.writer(tempfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#                 for row in student_reader:
-#                     if row != [] and row[1] not in selection:
-#                         student_writer.writerow(row)
-#             os.remove('names&numbers.csv')
-#             os.rename('tempfile.csv', 'names&numbers.csv')
-#
-#     def replace_student(self, *args):
-#
-#         # If a list item is selected
-#         if self.ids.students_list_view.adapter.selection:
-#
-#             # Get the text from the item selected
-#             selection = self.ids.students_list_view.adapter.selection[0].text
-#
-#             # Remove the matching item
-#             self.ids.students_list_view.adapter.data.remove(selection)
-#
-#             # Get the student name from the TextInputs
-#             student_name = self.first_name_text_input.text + " " + self.number_input.text
-#
-#             # Add the updated data to the list
-#             self.ids.students_list_view.adapter.data.extend([student_name])
-#
-#             # Reset the ListView
-#             self.ids.students_list_view._trigger_reset_populate()
-#
-#             with open('names&numbers.csv', mode='r') as names_numbers, open('tempfile.csv', 'w') as tempfile:
-#                 student_reader = csv.reader(names_numbers, delimiter=',')
-#                 student_writer = csv.writer(tempfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#                 for row in student_reader:
-#                     if row != []:
-#                         if row[1] not in selection:
-#                             student_writer.writerow(row)
-#                         elif row[1] in selection:
-#                             student_writer.writerow([self.first_name_text_input.text, self.number_input.text])
-#             os.remove('names&numbers.csv')
-#             os.rename('tempfile.csv', 'names&numbers.csv')
-
-
 class AccelerometerScreen(Screen):
     # this is the main widget that contains the game
     def __init__(self, **kwargs):
@@ -259,6 +168,33 @@ class TimerScreen(Screen):
             elif 'lat' in line:
                 lat = line.split('=')[1]
         return "https://www.google.nl/maps/@" + lat + "," + lon + ",20z"
+
+
+class ContactsScreen(Screen):
+
+    def __init__(self, **kwargs):
+        super(ContactsScreen, self).__init__(**kwargs)
+        self.phone_number_input_text = ObjectProperty(None)
+        self.phone_number_label = ObjectProperty(None)
+        Clock.schedule_once(lambda dt: self.setup())
+
+    def setup(self):
+        phone_number = "placeholder"
+        with open('phone_numbers.csv', mode='r') as phone_numbers:
+            student_reader = csv.reader(phone_numbers, delimiter=',')
+            for row in student_reader:
+                if row:
+                    if "PLACEHOLDER" in row:
+                        phone_number = "Please enter phone number"
+                    phone_number = row[0]
+        self.ids.phone_number_label.txt = phone_number
+
+    def enter_phone_number(self):
+        input_phone_number = self.ids.phone_number_input_text.text
+        self.ids.phone_number_label.txt = input_phone_number
+        with open('phone_numbers.csv', mode='w') as phone_numbers:
+            student_writer = csv.writer(phone_numbers, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            student_writer.writerow(input_phone_number)
 
 
 class ScreenManagement(ScreenManager):
